3d07a92512fd339b9edc84929e44d26983e96f09
kroliczek94 <kroliczek94>
2018-08-12 22:11:18 +0200

    Add project files.

---
+++ QuizApplication/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace QuizApplication.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ QuizApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="QuizApplication.Global" Language="C#" %>
